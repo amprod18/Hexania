fd8a6ace7bf1b5eeef25ac859f3ae5ce10cf7f70
amprod18 <martinezcuestaabraham@gmail.com>
2023-04-23 20:55:17 +0200

    installer fix + ico addition

--- main_front.py
+++ main_front.py
@@ -1,21 +1,24 @@
 import lib_installer
+lib_installer.installer()
 import sys
 import os
 import submenus_gui
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtGui
 from PyQt5.QtWidgets import QMainWindow
 
 
 class MainWindow(QMainWindow):
     def __init__(self, app):
         super().__init__()
-        self.setWindowTitle('Main Window')
+        icon = QtGui.QIcon(cwd + "icon.ico")
+        self.setWindowIcon(icon)
+        self.setWindowTitle('Hexania')
         self.main_menu = submenus_gui.Ui_main_menu(app, main_window)
         self.main_menu.show()
 
 
 if __name__ == "__main__":
-    lib_installer.installer()
+    cwd = os.getcwd()
     app = QtWidgets.QApplication(sys.argv)
     main_window = QtWidgets.QMainWindow()
     ui = MainWindow(app)
